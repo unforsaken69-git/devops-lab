f8c2b02ad0fe5365704c1bdd36d7d1ee915790cb
unforsaken69 <unforsaken69@gmail.com>
2025-11-17 23:36:40 +0800

    delete unneeded workflows, add new test_sample2.py

---
+++ tests/test_sample2.py
@@ -0,0 +1,7 @@
+def test_multiplication():
+    print ("this is test for multiplication")
+    assert 2 * 2 == 4
+
+def test_division():
+    print ("this is test for division")
+    assert 4 / 2 == 2
